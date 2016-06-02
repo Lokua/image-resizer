f4f41b92dabe462ccd09ba6135bc1c8697d90e3e
Lokua <lokua@lokua.net>
2016-06-02 01:29:16 -0500

    updated tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 import fs from 'mz/fs'
+import execa from 'execa'
 import assert from 'assert'
 import rimraf from 'rimraf-promise'
 import processImages from '../lib'
@@ -9,22 +10,25 @@ const config = {
   input: `${__dirname}/fixtures`,
   output: outDir,
   sizes: [200, 400, 600, 800],
-  debug: true,
+  debug: false,
   slugify: true
 }
 
-const test = async fn => {
+const test = async (desc, fn) => {
+  // console.log(`${desc} `)
   await fn()
+  // console.log(`after ${desc}`)
   await rimraf(outDir)
+  // return Promise.resolve(`(end) ${desc}`)
 }
 
-const sequence = async iterable => (
-  iterable.reduce((p, fn) => p.then(fn), Promise.resolve())
-)
+const sequence = async iterable => {
+  return iterable.reduce((p, fn) => p.then(fn), Promise.resolve())
+}
 
 sequence([
 
-  test(async () => {
+  test('directory', async () => {
     const results = await processImages(config)
     // console.log(JSON.stringify(results, null, 2))
 
@@ -32,13 +36,12 @@ sequence([
     assert(results[0].length === 4)
     assert(results[1].length === 4)
 
-    await results.map(async result => result.map(async ({ pathInfo }) => {
-      const exists = await fs.exists(`${pathInfo.dir}/${pathInfo.base}`)
-      assert(exists)
+    results.forEach(result => result.forEach(({ pathInfo }) => {
+      assert(fs.existsSync(`${pathInfo.dir}/${pathInfo.base}`))
     }))
   }),
 
-  test(async () => {
+  test('single file', async () => {
     const results = await processImages(
       Object.assign({}, config, { input: `${__dirname}/fixtures/tiger.jpg` })
     )
@@ -46,10 +49,25 @@ sequence([
     assert(results.length === 1)
     assert(results[0].length === 4)
 
-    await results[0].map(async ({ pathInfo }) => {
-      const exists = await fs.exists(`${pathInfo.dir}/${pathInfo.base}`)
-      assert(exists)
+    results[0].forEach(({ pathInfo}) => {
+      assert(fs.exists(`${pathInfo.dir}/${pathInfo.base}`))
     })
+  }),
+
+  test('cli', async () => {
+    const cmd = 'node ./lib -i ./test/fixtures -o ./test/__out -s 200,300'
+    const child = await execa.shell(cmd)
+    const results = JSON.parse(child.stdout)
+
+    assert(results.length === 2)
+    assert(results[0].length === 2)
+    assert(results[1].length === 2)
+
+    results.forEach(result => result.forEach(({ pathInfo }) => {
+      assert(fs.existsSync(`${pathInfo.dir}/${pathInfo.base}`))
+    }))
+
+    await rimraf(`${__dirname}/__out`)
   })
-]).then(() => console.log('Done'))
-  .catch(console.error.bind(console, 'caught err:'))
+]).then(() => console.log('done'))
+  .catch(err => console.error('err:', err))
